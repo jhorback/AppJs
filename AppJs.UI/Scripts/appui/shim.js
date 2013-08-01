bfe156b44c73394db540a8dee64d3476c5874fdc
John Horback <jhorback@gmail.com>
2013-07-31 22:25:24 -0400

    initial shim

---
+++ AppJs.UI/Scripts/appui/shim.js
@@ -0,0 +1,32 @@
+﻿
+var appui = appjs.module("appui");
+
+appui.construct("shim", ["shims", function (shims) {
+
+	return function (construct, name) {
+
+		shims.register(name, construct);
+		return construct;
+	};
+
+}]);
+
+
+appui.service("shims", function () {
+
+	var shims = {};
+
+	return {
+		register: function(name, shim) {
+			shims[name] = shim;
+		},
+		render: function(el, model) {
+			if (model && !model.toJSON) {
+				throw new Error("The model must implement toJSON.");
+			}
+			
+
+		}
+	};
+
+});
