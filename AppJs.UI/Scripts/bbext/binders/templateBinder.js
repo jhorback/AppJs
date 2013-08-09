dabd3375a5848a03ef3735b4c71ec41173468cb2
John Horback <jhorback@gmail.com>
2013-08-09 10:59:18 -0400

    working on binding

--- AppJs.UI/Scripts/bbext/binders/templateBinder.js
+++ AppJs.UI/Scripts/bbext/binders/templateBinder.js
@@ -10,10 +10,75 @@ module("bbext").register("templateRenderer", function () {
 	};
 });
 
+module("bbext").register("templateCache", ["$", "_", function ($, _) {
+
+	var templates = {};
+	
+
+	return {
+		cacheTemplateFor: function (name, templateEl) {
+			var html = $('<script/>').append(templateEl).html(),
+				templateFn = _.template(html);
+			templates[name] = templateFn;
+			return templateFn;
+		},
+		
+		getTemplateFor: function(name) {
+			var templateFn = templates[name],
+				templateEl;
+			
+			if (!templateFn) {
+				templateEl = $("[data-templatefor='" + name + "']");
+				if (templateEl.length === 0) {
+					throw "Template for '" + name + "' not found";
+				}
+				templateFn = this.cacheTemplateFor(name, templateEl);
+			}
+			
+			return templateFn;
+		}
+	};
+
+}]);
+
+/*
+Needs:
+templateRenderer.render(templateEl, potentialModel);
+	* called by app.render
+	* called by templateBinder.render
+	* templateEl will be placed in a script tag in order to extract the entire template
+		* then templateCache.cacheTemplateFor(viewName, html); will be called
+	* creates a view
+
+
+
+
+templateCache
+ * Something to cache a template with a name
+ * getTemplateFor("myApp.myView")
+	* will look in the cache or pull from the dom 
+ * cacheTemplateFor("name", 
+
+
+
+ renderViewExtension.render
+	* calls getTemplateFor(viewName)
+	* calls shims.render()
+
+
+*/
 
 module("bbext").shim("templateBinder",
 	["$", "templateRenderer",
 function ($, templateRenderer) {
+
+	function determineModel(templateEl, rootModel) {
+		
+	}
+
+	function renderTemplate(templateEl, model) {
+		
+	}
 
 	return {
 		// looks for data-templatefor
@@ -23,7 +88,7 @@ function ($, templateRenderer) {
 			
 			el = $(el);
 			el.find("[data-templatefor]").each(function (i, templateEl) {
-				var templateModel = model,
+				var templateModel = null,
 					bindTo;
 
 				templateEl = $(templateEl);
